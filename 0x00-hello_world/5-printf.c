144bd68a0999e309bcaf9e11c12f9156940bde18
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-05 10:03:50 -0700

    add script 4-6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description: printing with printf
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
