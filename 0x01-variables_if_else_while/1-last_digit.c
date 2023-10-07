8e52448d7a3f2b033def8f54536fd9e4ee01d9b2
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-07 08:49:44 -0700

    added 3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,25 +4,31 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n, x;
-    
+   
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	x = n % 10;
 	if (x > 5)
 	{
-	    printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	    	printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
 	else if(x == 0)
 	{
-	    printf("Last digit of %d is 0 and is 0\n", n);
+	   	printf("Last digit of %d is 0 and is 0\n", n);
 	}
 	else
 	{
-	    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+	    	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
+
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,17 +4,16 @@
  *
  * Return: Always 0 (success)
 */
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- * Description: Printing lines
-*/
+
 int main() {
-    char alp;
+    char alp, ALP;
     for (alp='a';alp<='z';alp++)
     {
 	    putchar(alp);
+    }
+    for (ALP='A';ALP<='Z';ALP++)
+    {
+	    putchar(ALP);
     }
 
     return 0;
