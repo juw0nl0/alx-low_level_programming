d9aa192a5bd2a1c4b73daedbb2c51f2f84bd09ad
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-07 09:32:33 -0700

    made formatting correction in 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	char alp;
-	for(alp='a';alp<='z';alp++)
+
+	for (alp = 'a'; alp <= 'z'; alp++)
 	{
-		if(alp!='q'&&alp!='e')
+		if (alp != 'q' && alp! = 'e')
 		{
-			putchar(alp);
+			putchar (alp);
 		}
 	}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
 
