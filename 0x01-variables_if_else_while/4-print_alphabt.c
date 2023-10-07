082628d4b0ec02b8910732ae651742753ad8775f
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-07 09:25:31 -0700

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alp;
+	for(alp='a';alp<='z';alp++)
+	{
+		if(alp!='q'&&alp!='e')
+		{
+			putchar(alp);
+		}
+	}
+	putchar('\n');
+	return(0)
+}
+
