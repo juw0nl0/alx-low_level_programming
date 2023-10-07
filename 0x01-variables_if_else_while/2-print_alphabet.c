80e7361480f93b983e4c387507c25ebaaf6c45d9
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-07 08:32:24 -0700

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main() {
+    char alp;
+    for (alp='a';alp<='z';alp++)
+    {
+        putchar(alp);
+    }
+    return 0;
+}
