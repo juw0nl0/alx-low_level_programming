a420fb2bd66fcdcb31e6e2dd76e16bfec5907647
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-07 21:09:18 -0700

    modify script 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 	for (x = 48; x < 58; x++)
 	{
 		putchar (x);
-		putchar (44);
-		putchar (32);
+		if (x != 57)
+		{
+			putchar (44);
+			putchar (32);
+		}
 	}
 	putchar ('\n');
 	return (0);
