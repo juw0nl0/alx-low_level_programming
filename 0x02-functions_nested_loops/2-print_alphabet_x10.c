ba72a1dd444b12ae7b2d5c6ec8b2128c1a5c272c
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-10 20:53:49 -0700

    add script 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	char alp;
+	int i;
+
+	for(i = 1; i <= 10; i++)
+	{
+		for(alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}	
