d83c019b2207e9dfbfe46e8a6a50e391aabe3990
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-11 21:17:08 -0700

    first commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	printf("Infinite loop incoming :(\n");
+
+	n = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
