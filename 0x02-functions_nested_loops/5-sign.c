58a664f3bbf8efa4f431d655b5c91393ca4c5ee6
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-11 20:11:00 -0700

    made correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,31 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_sign - checks if a character is positive,
- * negative or zero.
- * if positive it returns 1
- * els if zero it returns 0, 
- * else it return negative -1,
+ * print_sign - return 0 letter not 
+ * lowercase, 1 letter lowercase
  *
- * parameter n: is an interger
- * Return: if YES it returns 1, if zero it returns 0,
- * else it return -1.
+ * @n : number to check
 */
 int print_sign(int n)
 {
 	if (n < 0)
 	{
 		printf ("-1\n");
-		return (-1);
 	}
 	else if (n == 0)
 	{
 		printf ("0\n");
-		return (0);
 	}
 	else
 	{
 		printf ("+1\n");
-		return (1);
 	}
 }
