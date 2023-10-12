34fec1b115efdfd65965c810cf2094c3704e9afd
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-11 19:55:27 -0700

    made correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,14 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		printf("-1\n");
+		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
+		return (0);
 	}
 	else
 	{
-		printf("+1\n");
+		return (+1);
 	}
-	return (0);
 }
