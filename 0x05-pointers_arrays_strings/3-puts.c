a0192e394d4aa28ff225b737f48ce78843c8855d
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-17 04:37:54 -0700

    add script 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#inculde "main.h"
+
+/**
+ * _puts - a function that prints a string, followed by a new line
+ *
+ * @str: the input string of the function
+ *
+ * _putchar: print strings to std out
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
