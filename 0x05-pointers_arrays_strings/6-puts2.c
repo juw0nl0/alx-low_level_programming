ed4c136af3dcfb64e0f7cc9236f4e1b2c3788c6e
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-17 09:05:00 -0700

    add script 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * void puts2 - a function that prints every other character
+ * puts2 - a function that prints every other character
  * of a string, starting with the first character,
  * followed by a new line.
  *
- * @str: the input to the function
+ * @s: the input to the function
  *
  */
 void puts2(char *s)
