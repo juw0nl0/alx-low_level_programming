555d69ea04accc65ea76dc24b97e707c9fa92831
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-17 08:09:49 -0700

    add script 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@
  */
 void rev_string(char *s)
 {
-	int len = _strlen(s);
+	int len = strlen(s);
 	int i, j;
 	char temp;
 	
-	for (i = 0; j = len - 1; i < j; i++, j--)
+	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
 	}
-}
+u}
