c07c9e8211aa026daa5f379b15cea53af3a5fd59
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-17 03:05:19 -0700

    made formatting correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Update the value of a pointer
- *
- * n parameter - an interger
- * Return: updated value
+ * rest_to_98 -function that takes point and return an int 
+ * 
+ * @n: This is the entry
+ * 
  */
-
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
