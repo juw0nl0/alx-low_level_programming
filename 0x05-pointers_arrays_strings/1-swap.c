b5e93dd13d3ada7e5a6015a5640c1a26a1cdc022
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-17 03:40:31 -0700

    formattinf correction

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * void swap_int - a function that sway the value of two pointers
- * 
- * @a: entry parameter for swap function
- * @b: entry parameter for swap function
+ * void swap_int - function that sway the value of two pointers
+ *
+ * @a: first pointer entry
+ * @b: second pointer entry
  *
  */
 void swap_int(int *a, int *b)
