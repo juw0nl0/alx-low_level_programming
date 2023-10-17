84d74e7e78d67caedf490e1161db17b325113acc
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-17 03:12:01 -0700

    made formatting correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *reset_to_98 - function that takes point and return an int 
+ *reset_to_98 - function that takes point and return an int
  *
  *@n: This is the entry parameter
  *
