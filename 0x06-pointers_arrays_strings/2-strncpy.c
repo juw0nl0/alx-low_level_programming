ee9383bfb19a843cb5c78317024789b0dc5034e6
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-19 06:26:10 -0700

    add script 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  *_strncat - a function that concatenates two strings
+  *_strncpy - a function that concatenates two strings
   *
   *@dest: a first pointer parater to the function
   *@src: a second  pointer parater to the function
