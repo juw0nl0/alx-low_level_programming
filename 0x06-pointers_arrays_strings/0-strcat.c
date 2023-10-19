02b73285461e07ab94f8a726e621dedc8a1c3835
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-19 05:05:10 -0700

    add script 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *_strcat - a function that concatenates two strings
+  *
+  *@dest: a first pointer parater to the function
+  *@src: a second  pointer parater to the function
+  *Return: a concantenated string
+  */
+char *_strcat(char *dest, char *src)
+{
+	char *final_dest = dest;
+
+	while(*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest ++;
+		src++;
+	}
+	dest = ('\0');
+	return (final_dest);
+}
