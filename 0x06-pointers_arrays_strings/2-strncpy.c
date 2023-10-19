b177748146b8c0f784f03819b5892b50e0a0a756
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-19 06:24:01 -0700

    add script 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *_strncat - a function that concatenates two strings
+  *
+  *@dest: a first pointer parater to the function
+  *@src: a second  pointer parater to the function
+  *@n: nth characte from src input
+  *Return: a concantenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	char *final_dest = dest;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (final_dest);
+}
