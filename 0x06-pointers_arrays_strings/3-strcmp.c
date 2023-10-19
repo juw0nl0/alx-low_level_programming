52401c33433d41ccb59de041d20a60767ef8a475
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-19 07:15:08 -0700

    add scrip 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *_strcmp - a function that compare two strings
+  *
+  *@s1: a first pointer parater to the function
+  *@s2: a second  pointer parater to the function
+  *
+  *Return: an interget depending on the comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] < s2[i])
+		{
+			return (-1);
+		}
+		else if (si[i] > s2[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
