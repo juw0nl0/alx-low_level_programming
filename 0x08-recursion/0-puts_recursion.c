16549d13ce392e73ad1eaf2dadd18ea9ed40b840
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-26 09:16:19 -0700

    add script 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _put_recursion - a function that prints a string, followed by a new line.
+* _puts_recursion - a function that prints a string, followed by a new line.
 * @s: string to search
 * Return: a
-* 
+*
 */
 void _puts_recursion(char *s)
 {
