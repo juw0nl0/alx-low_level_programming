7fd4c865571b133a649e4a589b1cd2b7fffe29b8
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-27 00:11:44 -0700

    correction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,8 +14,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		return (_pow_recursion(x * *y));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
