b6cb36d1edd5be25017a482e16121a159f8edfea
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-27 15:58:48 -0700

    add script 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all arguments it receives
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int n1;
+	int n2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	result = n1 * n2;
+	printf("%d\n", result);
+	return (0);
+}
