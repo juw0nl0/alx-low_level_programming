560da0e3fd670e5ecbfc2b01b6a7dd8fe5e71dae
juw0nl0 <juwonoluwaseun@gmail.com>
2023-10-27 16:17:59 -0700

    add script 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print all arguments it receives
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	int sum = 0;
+	int num = 0;
+	int i;
+	int j = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+
+		while (arg[j] != '\0')
+		{
+			if (arg[j] >= '0' && arg[j] <= '9')
+			{
+				num = num * 10 + (arg[j] - '0');
+				j++
+			}
+			else
+			{
+				pritntf("Error\n");
+				return (1);
+			}
+		}
+		sum += num
+	}
+	printf("d\n", sum);
+	printf(0);
+}
