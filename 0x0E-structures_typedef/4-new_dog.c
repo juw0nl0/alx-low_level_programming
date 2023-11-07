a97a9308888a7a66c2a5bd6c8f591a154b9c8a01
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-07 09:09:01 -0800

    made correction script 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
-
 	newDog->age = age;
 
 	for (i = 0, c = 0; name[i] != '\0'; i++)
@@ -27,14 +26,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(newDog);
 		return (NULL);
 	}
-
 	for (i = 0; 1 ; i++)
 	{
 		(*(newDog->name + i)) = name[i];
 		if (name[i] == '\0')
 			break;
 	}
-
 	for (i = 0, c = 0; owner[i] != '\0'; i++)
 		c++;
 	newDog->owner = malloc(sizeof(char) * (c + 1));
@@ -44,13 +41,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(newDog->name);
 		return (NULL);
 	}
-
 	for (i = 0; 1 ; i++)
 	{
 		(*(newDog->owner + i)) = owner[i];
 		if (owner[i] == '\0')
 			break;
 	}
-
 	return (newDog);
 }
