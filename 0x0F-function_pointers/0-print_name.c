b2c6974aa4aee7a4ac2c43f8484a2509b9b5c7df
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-09 08:24:25 -0800

    add script 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - function that prints a name
+  * @name: a pointer to a charater data type
+  * @f:  a pointer funtion that point to a character and return nothing
+  * Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+	{
+		return;
+	}
+	f(name)
+}
