5d90cc24e1e79bfd29e8d941c83a44a1aa40a3a2
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-09 12:22:45 -0800

    add script 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (!action || !array)
+	{
 		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
