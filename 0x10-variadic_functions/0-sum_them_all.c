5e98ba7b37b64b0acf48f3c81d40341c9b1daa06
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-10 08:14:50 -0800

    correction 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,5 @@
 #include <stdarg.h>
-#include "variadic_function.h"
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - adds num
@@ -8,19 +8,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
 	int sum = 0;
+	unsigned int i;
 
 	if (n == 0)
+	{
 		return (0);
+	}
+	va_list args;
 
-	va_start(valist, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-
-	va_end(valist);
-
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
 	return (sum);
 }
