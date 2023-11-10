7b940b827f6967b7b5e3e78c3b2e740002ca4b03
juw0nl0 <juwonoluwaseun@gmail.com>
2023-11-10 08:10:56 -0800

    correction 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,21 +8,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	va_list valist;
 	unsigned int i;
+	int sum = 0;
 
 	if (n == 0)
-	{
 		return (0);
-	}
-	va_list args;
 
-	va_start(args, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(args, int);
-	}
-	va_end(args);
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
 	return (sum);
 }
